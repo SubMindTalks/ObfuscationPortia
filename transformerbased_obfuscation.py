96079e7c1e9a52ed4ae1ec54c4adc067c002895a
SubmindTalks <submindtalks@gmail.com>
2024-11-30 16:29:04 +0100

    preparing obfuscation

---
+++ transformerbased_obfuscation.py
@@ -0,0 +1,10 @@
+use hugginface transformers to perform code obfuscation.
+- substitute verbs for synonyms
+- change camelcase, snakecase, pascalcase, kebabcase
+- obfuscate coding style
+- remove empty lines and comments and docstrings and type hints
+- Rename all variables to mathematical symbols only
+
+Once completed run the code again to ensure that it is still running and working and producing the same result as before
+
+Do this for all python files in Baseline and save a mirrored filestructured result to Baseline_obfuscated. For obfuscated files that no longer do the same or can run/compile either retry and fix the errors, or give up and delete the file from Baseline_obfuscated
